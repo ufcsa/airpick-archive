d66d886a247693c064c5df5ac88299b7a49d4656
Zhongkai Liu <liu676785882@gmail.com>
2018-05-21 11:59:39 -0400

    Added new db link, deleted package-lock.json

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://ufcsa:ufcsadb@ds231090.mlab.com:31090/airpick',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
