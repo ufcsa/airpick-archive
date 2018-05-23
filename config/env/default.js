f195cc9e9d682c6530325c1497de09ff5c6df8be
Yinghan <ma.yingha@ufl.edu>
2018-05-23 22:37:36 +0800

    Signup page v0.1

--- config/env/default.js
+++ config/env/default.js
@@ -68,7 +68,7 @@ module.exports = {
     owasp: {
       allowPassphrases: true,
       maxLength: 128,
-      minLength: 10,
+      minLength: 6,
       minPhraseLength: 20,
       minOptionalTestsToPass: 4
     }
