e6ef902850c2657b2fc2047ceaabfde3f682a2ec
Shida <shidayang@ufl.edu>
2018-05-23 22:17:30 -0400

    change color

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -15,13 +15,16 @@
         var detail = this.nextElementSibling;
         if (detail.style.display === "block") {
           detail.style.display = "none";
+          this.style.background = "rgba(250,70,22,0.5)";
+          detail.style.background = "rgba(250,70,22,0.5)";
         }
         else {
           detail.style.display = "block";
+          this.style.background = "rgba(250,70,22,0.7)";
+          detail.style.background = "rgba(250,70,22,0.7)";
         }
       });
     }
-
   }
 
 }
