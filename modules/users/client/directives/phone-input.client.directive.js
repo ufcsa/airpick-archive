c0c8af74acfad3c219c577811381aa5c6c6b5fc5
Zhongkai Liu <liu676785882@gmail.com>
2018-05-30 18:51:30 -0400

    Fix bugs

--- modules/users/client/directives/phone-input.client.directive.js
+++ modules/users/client/directives/phone-input.client.directive.js
@@ -5,7 +5,7 @@
     .module('users')
     .directive('phoneInput', phoneInput);
 
-  phoneInput.$inject = [$filter, $browser];
+  phoneInput.$inject = ['$filter', '$browser'];
 
   function phoneInput($filter, $browser) {
     return {
@@ -24,23 +24,23 @@
         });
 
         ngModelCtrl.$render = function() {
-            $element.val($filter('tel')(ngModelCtrl.$viewValue, false));
+            element.val($filter('tel')(ngModelCtrl.$viewValue, false));
         };
 
-      element.bind('change', listener);
-      element.bind('keydown', function(event) {
-          var key = event.keyCode;
-          // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
-          // This lets us support copy and paste too
-          if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
-              return;
-          }
-          $browser.defer(listener); // Have to do this or changes don't get picked up properly
-      });
+        element.bind('change', listener);
+        element.bind('keydown', function(event) {
+            var key = event.keyCode;
+            // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+            // This lets us support copy and paste too
+            if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
+                return;
+            }
+            $browser.defer(listener); // Have to do this or changes don't get picked up properly
+        });
 
-      element.bind('paste cut', function() {
-          $browser.defer(listener);
-      });
+        element.bind('paste cut', function() {
+            $browser.defer(listener);
+        });
       }
 
     };
--- modules/users/client/filters/tel.client.filter.js
+++ modules/users/client/filters/tel.client.filter.js
@@ -3,7 +3,7 @@
 
   angular
     .module('users')
-    .filter('tel', tel);
+    .filter('tel', filter);
 
   filter.$inject = [/*Example: '$state', '$window' */];
 
