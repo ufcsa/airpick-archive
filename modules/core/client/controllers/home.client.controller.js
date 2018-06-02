357a03dfda126ec469b4eff7f9e4b08d6ee8bd6d
Jason <cyhy462702086@gmail.com>
2018-06-02 18:35:38 -0400

    6.2 update

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -15,13 +15,13 @@
         var detail = this.nextElementSibling;
         if (detail.style.display === "block") {
           detail.style.display = "none";
-          this.style.background = "rgba(250,70,22,0.5)";
-          detail.style.background = "rgba(250,70,22,0.5)";
+          this.style.background = "rgba(250,70,22,1)";
+          detail.style.background = "rgba(250,70,22,1)";
         }
         else {
           detail.style.display = "block";
-          this.style.background = "rgba(250,70,22,0.7)";
-          detail.style.background = "rgba(250,70,22,0.7)";
+          this.style.background = "rgba(250,70,22,1)";
+          detail.style.background = "rgba(250,70,22,1)";
         }
       });
     }
