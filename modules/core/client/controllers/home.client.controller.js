5ca38b2570841a8325a2b114b3907f70a730a82e
Yinghan <mayinghan97@gmail.com>
2018-06-07 16:50:56 +0800

    fixed small errors

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -9,16 +9,16 @@
     var vm = this;
 
     //open or close the collapsible
-    var colls = document.getElementsByClassName("collapsible");
+    var colls = document.getElementsByClassName('collapsible');
     for (var i = 0; i < colls.length; i++) {
-      colls[i].addEventListener("click", function () {
+      colls[i].addEventListener('click', function () {
         var detail = this.nextElementSibling;
         if (detail.style.display === 'block') {
           detail.style.display = 'none';
-          this.classList.remove("active");
+          this.classList.remove('active');
         } else {
           detail.style.display = 'block';
-          this.classList.add("active");
+          this.classList.add('active');
         }
       });
     }
