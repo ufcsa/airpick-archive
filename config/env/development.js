52de5c12221a29423c0e4dae13570e6e33d31806
LeChn <33189757+LeChn@users.noreply.github.com>
2018-06-20 17:26:08 -0700

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@ var defaultEnvConfig = require('./default'),
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://student:classcen3031@ds045465.mlab.com:45465/class',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://ufcsa:ufcsadb@ds231090.mlab.com:31090/airpick',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
