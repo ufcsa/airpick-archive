7cb37dd417bb16db61491694887034bb1296f8a9
Zhongkai Liu <liu676785882@gmail.com>
2018-08-25 13:40:30 -0400

    orientation email sending bug fix

--- scripts/orientationEmail.py
+++ scripts/orientationEmail.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
 import requests
+import time
 
 apiURL = "http://localhost:3000/api/admin/orientation/email"
 
@@ -12,8 +13,11 @@ for row in ws.rows:
     if first:
         first = False
         continue
+    if row[3].value == None or row[3].value == "" or len(row[3].value) < 4:
+        continue
     body = {}
     body["name"] = row[2].value
     body["table"] = row[7].value
     body["receiver"] = row[3].value
     resp = requests.post(apiURL, json=body)
+    time.sleep(5)
